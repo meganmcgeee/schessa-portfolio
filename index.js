1f1c8ad6b427cc39fa743b41c910c96e4e3c19bd
meganmcgeee <meganpmcgee@gmail.com>
2016-09-21 16:27:13 -0700

    Added color and new sizing to grid framework.

--- index.js
+++ index.js
@@ -2,21 +2,7 @@
 
 // external js: isotope.pkgd.js
 
-var grid = document.querySelector('.grid');
-var iso = new Isotope( grid, {
-  itemSelector: '.grid-item',
-  masonry: {
-    columnWidth: 80
-  }
-});
-
-grid.addEventListener( 'click', function( event ) {
-  var target = event.target;
-  // only click on itemContent
-  if ( !target.classList.contains('grid-item-content') ) {
-    return;
-  }
-  var itemElem = target.parentNode;
-  itemElem.classList.toggle('is-expanded');
-  iso.layout();
+$('.grid').isotope({
+  layoutMode: 'packery',
+  itemSelector: '.grid-item'
 });
