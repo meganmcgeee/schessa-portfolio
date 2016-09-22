98d551db8ebb35087532788cb727497db6d8aa6c
meganmcgeee <meganpmcgee@gmail.com>
2016-09-21 20:23:03 -0700

    Experimenting with isotope.

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ $('.grid').isotope({
   layoutMode: 'packery',
   itemSelector: '.grid-item'
 });
+
+$('.grid-item').click(function(){
+  var $this = $(this),
+      tileStyle = $this.hasClass('big') ? { width: 50, height: 50} : { width: 170, height: 110};
+  $this.toggleClass('big');
+
+  $this.find('.grid-content').stop().animate( tileStyle );
+
+  $container.isotope( 'reLayout' )
+
+});
