719ac8fae4e47385d6776ee6e3f508104f872538
Matthew Jesuele <mjesuele@gmail.com>
2016-11-08 16:26:35 -0800

    Using babel-eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
+    "parser": "babel-eslint",
     "env": {
         "browser": true,
-        "es6": true
+        "node": true,
+    },
+    "parserOptions": {
+      "ecmaVersion": 7,
     },
     "extends": "eslint:recommended",
     "rules": {
