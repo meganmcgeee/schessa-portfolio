54dfd6a8a9f5ef0ca4ea3d7c910a3124d742190c
Matthew Jesuele <mjesuele@gmail.com>
2017-01-26 15:25:58 -0800

    Improved the script for creating thumbnails.

--- scripts/makeThumbnails.js
+++ scripts/makeThumbnails.js
@@ -2,16 +2,32 @@ const easyimg = require('easyimage');
 const glob = require('glob');
 
 glob('../public/images/*.jpg', {}, (err, files) =>
-  files.forEach(src =>
-    easyimg.thumbnail({
+  files.forEach(async src => {
+    const { height, width } = await easyimg.info(src);
+    const size = Math.min(1200, Math.min(height, width));
+    // console.log(height, width, size);
+    const img = await easyimg.thumbnail({
       src,
       dst: `../public/images/thumbnails/${src.split('/').pop()}`,
-      height: 800,
+      height: size,
       quality: 70,
-      width: 800,
-    }).then(
-      img => console.log('made thumbnail for', img),
-      console.error
-    )
-  )
+      width: size,
+    });
+    console.log('made thumbnail for', img);
+  })
 );
+
+const logFunc = f => (...args) => {
+  console.log('Calling with arguments:', ...args);
+  const result = f(...args);
+  console.log('Result:', result);
+  return result;
+};
+
+const _reverse = logFunc(
+  ([head, ...tail], reversed = []) => head ?
+    _reverse(tail, [head, ...reversed]) :
+    reversed
+);
+
+const reverse = arr => _reverse(arr, []);
